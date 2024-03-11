8d09829d41c8bbfdae3b0d7f52f75cf78b1532f0
jobran4799 <jobran4799@gmail.com>
2024-03-11 11:29:49 +0200

    add ci.yml to git

--- tests/Test_Runer.py
+++ tests/Test_Runer.py
@@ -2,10 +2,10 @@
 import json
 import unittest
 from concurrent.futures import ThreadPoolExecutor
-from infra.Brawser_Wrapper import BrowserWrapper
-from tests.Log_In_Page_Test import Login_page_test
-from tests.Main_Page_Test import Main_page_test
-from tests.Projects_List_Page_Test import Project_List_Page_Test
+from infra.UI.Brawser_Wrapper import BrowserWrapper
+from tests.UI.Log_In_Page_Test import Login_page_test
+from tests.UI.Main_Page_Test import Main_page_test
+from tests.UI.Projects_List_Page_Test import Project_List_Page_Test
 
 try:
     with open('../Config_Manegre/config.json') as f:
